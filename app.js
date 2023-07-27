700771899db43aeb52e3631118c05ce772ec2f0b
Tsubasa-Komiyama <armis12365@gmail.com>
2023-07-27 12:58:11 +0900

    トリボナッチ数列に変更

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
 'use strict'
+// const memo = new Map();
+// memo.set(0,0);
+// memo.set(1,1);
+// function fib(n) {
+//   if(memo.has(n)){
+//     return memo.get(n);
+//   }
+//   const value = fib(n-1) + fib(n-2);
+//   memo.set(n, value);
+//   return value;
+// }
+// const length = 100;
+// for(let i = 0; i <= length; i++){
+//   console.log(fib(i));
+// }
+
 const memo = new Map();
-memo.set(0,0);
-memo.set(1,1);
-function fib(n) {
-  if(memo.has(n)){
+memo.set(0, 0);
+memo.set(1, 0);
+memo.set(2, 1);
+function tor(n) {
+  if (memo.has(n)) {
     return memo.get(n);
   }
-  const value = fib(n-1) + fib(n-2);
+  const value = tor(n - 1) + tor(n - 2) + tor(n - 3);
   memo.set(n, value);
   return value;
 }
-const length = 100;
-for(let i = 0; i <= length; i++){
-  console.log(fib(i));
+const length = 40;
+for (let i = 0; i <= length; i++) {
+  console.log(tor(i));
 }
